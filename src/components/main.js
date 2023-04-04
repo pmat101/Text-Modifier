91e2691afd51ff732deeaca9f04c06ce7a78df17
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-04-04 23:55:51 +0530

    working on Bootstrap Modal to replace basic prompt

--- src/components/main.js
+++ src/components/main.js
@@ -60,9 +60,53 @@ export default function Main(props) {
           onChange={handleChange} // Allows user to type even after button click
         ></textarea>
         <label htmlFor="floatingTextarea">Enter your text here:</label>
-        <button className="btn btn-primary m-2" onClick={dummy}>
+        <button
+          type="button"
+          className="btn btn-primary m-2"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+          onClick={dummy}
+        >
           Generate random text
         </button>
+        <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          tabindex="-1"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Understood
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
         <button className={`btn btn-${textColour} m-2`} onClick={doUp}>
           Make Uppercase
         </button>
